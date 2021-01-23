bd9058b67a1cdeced5f864d2d3350c1b486f7c3d
Hugh Bromund <hughbromund44@gmail.com>
2021-01-22 21:29:40 -0500

    Initialized XCode Project

---
+++ Justifi/Shared/JustifiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustifiApp.swift
+//  Shared
+//
+//  Created by Hugh Bromund on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct JustifiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
