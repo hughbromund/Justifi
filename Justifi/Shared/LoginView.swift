4c53d5aee31f097f33b536c32fdfe2a47f704971
Aditya Naik <adityan9900@gmail.com>
2021-01-24 07:38:32 -0500

    COlor

--- Justifi/Shared/LoginView.swift
+++ Justifi/Shared/LoginView.swift
@@ -25,72 +25,76 @@ struct LoginView: View {
     @State var showSignUp : Bool = false
     
     var body: some View {
-        VStack {
-            Text("Login")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .padding(.bottom, 20)
-            TextField("Username", text: $username)
-                .padding()
-                .autocapitalization(.none)
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
-            SecureField("Password", text: $password)
-                .padding()
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
-            Text("Sign Up").padding(.bottom, 20).onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
-                showSignUp = true
-            }).sheet(isPresented: $showSignUp, content: {
-                SignUpView(showSignUp: $showSignUp)
-            })
-            Button(action: {
-                    print("Button tapped")
-                    isLoading = true
-                    Request {
-                        Url("https://justifi.uc.r.appspot.com/api/auth/login")
-                        Method(.post)
-                        Header.ContentType(.json)
-                        RequestBody([
-                            "username": username,
-                            "password": password
-                        ])
-                    }.onData { data in
-                        do {
-                            try print("onData: \(Json(data).accessToken)")
-                            try print(Json(data).accessToken.string)
-                            accessToken = try! Json(data).accessToken.string
-                            parentUsername = try! Json(data).username.string
-                            loginSuccess = true
-                            isLoading = false
-                        } catch {
-                            print("Error")
-                        }
-                        
-                    }
-                    .onError { error in
-                        print("onError: \(error)")
-                        isLoading = false
-                    }
-                    .call()
-            }) {
-                Text(isLoading ? "LOADING..." :  "LOGIN")
-                    .font(.headline)
-                    .foregroundColor(.white)
+        ZStack {
+            Color(red: 247.0/255.0, green: 244.0/255.0, blue: 243.0/255.0, opacity: 1.0).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 20)
+                    .foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                TextField("Username", text: $username)
                     .padding()
-                    .frame(width: 220, height: 60)
-                    .background(loginSuccess ? Color.green : Color.blue)
-                    .cornerRadius(15.0)
+                    .autocapitalization(.none)
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                    .foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                SecureField("Password", text: $password)
+                    .padding()
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                    .foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                Text("Sign Up").padding(.bottom, 20).onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                    showSignUp = true
+                }).sheet(isPresented: $showSignUp, content: {
+                    SignUpView(showSignUp: $showSignUp)
+                }).foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                Button(action: {
+                        print("Button tapped")
+                        isLoading = true
+                        Request {
+                            Url("https://justifi.uc.r.appspot.com/api/auth/login")
+                            Method(.post)
+                            Header.ContentType(.json)
+                            RequestBody([
+                                "username": username,
+                                "password": password
+                            ])
+                        }.onData { data in
+                            do {
+                                try print("onData: \(Json(data).accessToken)")
+                                try print(Json(data).accessToken.string)
+                                accessToken = try! Json(data).accessToken.string
+                                parentUsername = try! Json(data).username.string
+                                loginSuccess = true
+                                isLoading = false
+                            } catch {
+                                print("Error")
+                            }
+                            
+                        }
+                        .onError { error in
+                            print("onError: \(error)")
+                            isLoading = false
+                        }
+                        .call()
+                }) {
+                    Text(isLoading ? "LOADING..." :  "LOGIN")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 220, height: 60)
+                        .background(loginSuccess ? Color.green : Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                        .cornerRadius(15.0)
+                }
             }
-            Text("\(username)")
-            Text("\(password)")
-
         }
     }
+    
 }
 
 //struct LoginView_Previews: PreviewProvider {
--- Justifi/Shared/SignUpView.swift
+++ Justifi/Shared/SignUpView.swift
@@ -22,61 +22,66 @@ struct SignUpView: View {
     @State var isLoading : Bool = false
     
     var body: some View {
-        VStack {
-            Text("Sign Up")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .padding(.bottom, 20)
-            TextField("Username", text: $username)
-                .padding()
-                .autocapitalization(.none)
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
-            SecureField("Password", text: $password)
-                .padding()
-                .cornerRadius(5.0)
-                .padding(.bottom, 20)
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
-            Button(action: {
-                    print("Button tapped")
-                    isLoading = true
-                    Request {
-                        Url("https://justifi.uc.r.appspot.com/api/auth/signup")
-                        Method(.post)
-                        Header.ContentType(.json)
-                        RequestBody([
-                            "username": username,
-                            "password": password
-                        ])
-                    }.onData { data in
-                        do {
-                            try print("onData: \(Json(data))")
-                            signUpSuccess = true
-                            isLoading = false
-                        } catch {
-                            print("Error")
-                        }
-                    }
-                    .onError { error in
-                        print("onError: \(error)")
-                        isLoading = false
-                    }
-                    .call()
-                    showSignUp = false
-            }) {
-                Text(isLoading ? "LOADING..." :  "SIGN UP")
-                    .font(.headline)
-                    .foregroundColor(.white)
+    
+        ZStack {
+            Color(red: 247.0/255.0, green: 244.0/255.0, blue: 243.0/255.0, opacity: 1.0).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Sign Up")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 20)
+                    .foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                TextField("Username", text: $username)
                     .padding()
-                    .frame(width: 220, height: 60)
-                    .background(signUpSuccess ? Color.green : Color.blue)
-                    .cornerRadius(15.0)
+                    .autocapitalization(.none)
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                    .foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                SecureField("Password", text: $password)
+                    .padding()
+                    .cornerRadius(5.0)
+                    .padding(.bottom, 20)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                    .foregroundColor(Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                Button(action: {
+                        print("Button tapped")
+                        isLoading = true
+                        Request {
+                            Url("https://justifi.uc.r.appspot.com/api/auth/signup")
+                            Method(.post)
+                            Header.ContentType(.json)
+                            RequestBody([
+                                "username": username,
+                                "password": password
+                            ])
+                        }.onData { data in
+                            do {
+                                try print("onData: \(Json(data))")
+                                signUpSuccess = true
+                                isLoading = false
+                            } catch {
+                                print("Error")
+                            }
+                        }
+                        .onError { error in
+                            print("onError: \(error)")
+                            isLoading = false
+                        }
+                        .call()
+                        showSignUp = false
+                }) {
+                    Text(isLoading ? "LOADING..." :  "SIGN UP")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 220, height: 60)
+                        .background(signUpSuccess ? Color.green : Color(red: 19 / 255, green: 41 / 255, blue: 61 / 255))
+                        .cornerRadius(15.0)
+                }
             }
-            Text("\(username)")
-            Text("\(password)")
         }
     }
 }
