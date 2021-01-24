35b6044ea9e87a9ddac1dd0731a32e745aa22d4e
Hugh Bromund <hughbromund44@gmail.com>
2021-01-24 02:24:13 -0500

    More Updatets to the frontend

--- Justifi/Shared/ScrollView.swift
+++ Justifi/Shared/ScrollView.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 import SwiftUIPager
+import Alamofire
+import Request
+import Json
 
 struct ScrollView: View {
     
+    @Binding var accessToken: String
+    
     @StateObject var page: Page = .first()
+    
+    
     // @ObservedObject var page: Page = .first()
     var items = Array(0..<3)
+    
     var videoViews = Array([VideoView(pageNum: 0), VideoView(pageNum: 1), VideoView(pageNum: 2)])
     
     var body: some View {
@@ -23,8 +31,6 @@ struct ScrollView: View {
                   content: { index in
                       // create a page based on the data passed
                     ZStack {
-                        Rectangle()
-                            .fill(Color.yellow)
                         videoViews[index]
                     }
                     .cornerRadius(5)
@@ -32,21 +38,55 @@ struct ScrollView: View {
                   }).vertical()
                 .loopPages()
                 .onPageChanged({ page in
-                                print("Page changed to: \(page)")})
-                .contentLoadingPolicy(.eager)
-        VStack {
-            HStack {
-                Text("Justifi").font(.largeTitle).bold().padding()
+                    if (page == videoViews.count - 2) {
+                        // Add More pages
+                    }
+                    print("Page changed to: \(page)")})
+                .contentLoadingPolicy(.lazy(recyclingRatio: 5))
+            VStack {
+                HStack {
+                    Text("Justifi").font(.largeTitle).bold().padding()
+                    Spacer()
+                }
                 Spacer()
             }
-            Spacer()
-        }
-        }
+        }.onAppear(perform: {
+            print("Home Appeared")
+            Request {
+                Url("https://justifi.uc.r.appspot.com/api/video/updateFeed")
+                Method(.post)
+                Header.Any(key: "x-access-token", value: accessToken)
+                Header.ContentType(.json)
+            }.onData { data in
+                print("Update Feed Success")
+                Request {
+                    Url("https://justifi.uc.r.appspot.com/api/video/list")
+                    Method(.get)
+                    Header.Any(key: "x-access-token", value: accessToken)
+                    Header.ContentType(.json)
+                }.onData { data in
+                    print("list of video")
+                    do {
+                        try print(Json(data))
+                    } catch {
+                        print("Error")
+                    }
+                   
+
+                }.onError{ error in
+                    print(error)
+                }.call()
+            }.onError{ error in
+                print(error)
+            }.call()
+        })
     }
 }
 
 struct ScrollView_Previews: PreviewProvider {
+    @State static var tempToken : String = "HughBromund"
+    
     static var previews: some View {
-        ScrollView()
+        ScrollView(accessToken: $tempToken)
     }
 }
